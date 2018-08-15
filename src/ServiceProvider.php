8ff83ea653bbe5f171a2476f691df0f13505abc7
Dmitry Borzyonok <dima.matters@gmail.com>
2018-08-15 14:22:48 +0300

    Move main logic to manager

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,9 +4,9 @@ declare(strict_types = 1);
 
 namespace McMatters\LaravelScheduleTerminating;
 
-use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 use McMatters\LaravelScheduleTerminating\Console\Commands\Terminate;
+use McMatters\LaravelScheduleTerminating\Managers\ScheduleManager;
 use const true;
 
 /**
@@ -27,7 +27,7 @@ class ServiceProvider extends BaseServiceProvider
     public function register()
     {
         $this->app->singleton('command.schedule.terminate', function ($app) {
-            return new Terminate($app->make(Schedule::class));
+            return new Terminate(new ScheduleManager($app));
         });
 
         $this->commands(['command.schedule.terminate']);
